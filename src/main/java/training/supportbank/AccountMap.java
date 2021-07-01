7d512ff0133b0c7075e571cb9f480fea414b2c74
nkadiadrian <adriannkadi@gmail.com>
2021-07-01 17:34:01 +0200

    Added functionality to read csv and add accounts and transactions

---
+++ src/main/java/training/supportbank/AccountMap.java
@@ -0,0 +1,21 @@
+package training.supportbank;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AccountMap {
+    Map<String, Account> accountMap  = new HashMap<>();
+
+    public AccountMap() {
+    }
+
+    public void addTransaction(Transaction transaction) {
+        String to = transaction.getTo();
+        String from = transaction.getFrom();
+        accountMap.putIfAbsent(from, new Account(from));
+        accountMap.putIfAbsent(to, new Account(to));
+        accountMap.get(from).addTransactionFrom(transaction);
+        accountMap.get(to).addTransactionTo(transaction);
+    }
+
+}
